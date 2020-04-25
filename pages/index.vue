4f715bfefb8dd9b14b31c4dbbd62aaee2b5f35cc
Ritsuki KOKUBO <ritsu2891@gmail.com>
2020-04-25 15:27:26 +0900

    [追加] Nuxt.js プロジェクト初期化

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to POICA WEB
+        </v-card-title>
+        <v-card-text>
+          <p>This is Top Page.</p>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/"
+          >
+            OK
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
