c65ef664b5b76c356490d92fe6403bc4be7cb161
Ritsuki KOKUBO <ritsu2891@gmail.com>
2020-05-04 23:28:28 +0900

    [追加] 管理しているカードマスタ一覧API

--- repos/CardMasterRepo.js
+++ repos/CardMasterRepo.js
@@ -1,14 +1,25 @@
-export { getByRegToken }
+export { list, getByRegToken }
 
-import { api } from '~/repos/common.js';
+import { api, authorizedApi } from '~/repos/common.js';
 import CardMaster from '~/models/CardMaster.model.js';
 
 const Cookies = require('js-cookie');
 
 // const urljoin = require('url-join');
 const endpoints = {
+  list: 'cardmasters/list',
   getByRegToken: 'cardmasters/byRegToken'
 };
+
+async function list() {
+  const res = await authorizedApi().get(endpoints.list);
+  const data = res.data;
+  if (data.result == 'ok') {
+    return data.masters.map((master) => new CardMaster(master));
+  } else {
+    return null;
+  }
+}
 
 async function getByRegToken(regToken) {
   const res = await api().get(endpoints.getByRegToken, {
