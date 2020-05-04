3d4af34b15badafc7f69b53b73c9b3caf294a091
Ritsuki KOKUBO <ritsu2891@gmail.com>
2020-05-04 14:30:21 +0900

    [修正] リポジトリの返り値変更等に伴う対応

--- pages/registered-cards.vue
+++ pages/registered-cards.vue
@@ -3,7 +3,7 @@
     <h1>登録済カード一覧</h1>
     <v-row>
       <v-col cols="12" sm="6" lg="4" v-for="card in cards" :key="card.id">
-        <SimplePointCard :title="card.master + ''" :point="card.point" class="card elevation-5"></SimplePointCard>
+        <SimplePointCard :title="card.masterId + ''" :point="card.point" class="card elevation-5"></SimplePointCard>
       </v-col>
     </v-row>
   </div>
@@ -29,7 +29,7 @@ export default {
   beforeRouteEnter(to, from, next) {
     next(vm => {
       RegisterCardRepo.getAll().then(res => {
-        vm.cards = res.data;
+        vm.cards = res.data.cards;
       })
     })
   }
