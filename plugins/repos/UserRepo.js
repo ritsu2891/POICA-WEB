fd0b917219f7c6c70c5005c3dd066f7e37635e9b
Ritsuki KOKUBO <ritsu2891@gmail.com>
2020-05-20 17:50:13 +0900

    [追加] ユーザID関連のAPIを追加

--- plugins/repos/UserRepo.js
+++ plugins/repos/UserRepo.js
@@ -1,10 +1,23 @@
 const endpoints = {
+  checkIdDupl: 'users/checkIdDupl',
   myProfile: 'users/myProfile',
   searchByDisplayName: 'users/byDisplayName',
+  searchByUserId: 'users/searchByUserId',
 }
 
 export default ({ app }, inject) => {
   inject('userRepo', {
+    checkIdDupl: async function (userId) {
+      const res = await app.$api().post(endpoints.checkIdDupl, {
+        userId: userId
+      });
+      const data = res.data;
+      if (data.result == 'ok') {
+        return data.duplicate;
+      } else {
+        return false;
+      }
+    },
     myProfile: async function () {
       const res = await app.$authorizedApi().get(endpoints.myProfile);
       const data = res.data;
@@ -13,6 +26,10 @@ export default ({ app }, inject) => {
       } else {
         return null;
       }
+    },
+    updateMyProfile: async function (profile) {
+      const res = await app.$authorizedApi().post(endpoints.myProfile, profile);
+      return res.data.result == 'ok';
     },
     searchByDisplayName: async function (displayName) {
       const res = await app.$api().get(endpoints.searchByDisplayName, {
@@ -27,5 +44,18 @@ export default ({ app }, inject) => {
         return null;
       }
     },
+    searchByUserId: async function (userId) {
+      const res = await app.$api().get(endpoints.searchByUserId, {
+        params: {
+          userId: userId
+        }
+      });
+      const data = res.data;
+      if (data.result == 'ok') {
+        return data.users;
+      } else {
+        return null;
+      }
+    }
   });
 }
